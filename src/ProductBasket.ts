d31723811d665bc49a0a2ee0e5c26259ceee2244
Dr.klo <kulinchik.leonid@gmail.com>
2019-10-24 20:55:04 +0300

    init

---
+++ src/ProductBasket.ts
@@ -0,0 +1,26 @@
+﻿import {IProduct} from "@common/api/IProduct";
+import {IUser} from "../../Contracts/src/api/IUser";
+
+export class ProductBasket {
+    private products:  IProduct[];
+    constructor(private owner: IUser){
+        this.products = new Array();
+    }
+    public Add(product: IProduct): void{
+        let existingProduct = this.products.find(p=>p.id == product.id);
+        if (existingProduct) {
+            existingProduct.count+=product.count;
+        } else  {
+            this.products.push(product);
+        }
+    }
+    public Remove(id: number){
+        const index= this.products.findIndex(p=> p.id == id);
+        if (index>=0) {
+            this.products.splice(index, 1);
+        }
+    }
+    public getList(): IProduct[] {
+        return this.products.slice();
+    }
+}
